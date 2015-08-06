6f5555c49320e492d1a8b003a18fb69b286817d1
Vlad <dzyubavlad@gmail.com>
2015-08-06 14:47:15 +0300

    Initial commit

---
+++ com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by Dzyub_000 on 06.08.2015.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
